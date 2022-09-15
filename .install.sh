f81bda389b45f0c70f8b8602ad05c1a7e674c6a2
Juho Kajava <44448646+ohtohalla@users.noreply.github.com>
2022-09-16 00:40:19 +0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -95,10 +95,12 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Copying and checking out configuration files
-alias config='/usr/bin/git --git-dir=$HOME/.dots --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
 echo ".dots" >> .gitignore
 git clone --bare git@github.com:ohtohalla/macos_dots.git $HOME/.dots/
+alias config='/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
+config checkout
 
 # Python Packages
 echo "Installing Python Packages..."
