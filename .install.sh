da32325b5c56ada955aef81c75b3f476490a03bf
Juho Kajava <44448646+ohtohalla@users.noreply.github.com>
2022-09-17 01:15:42 +0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -6,7 +6,7 @@ xcode-select --install
 
 # Install Brew
 echo "Installing Brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
